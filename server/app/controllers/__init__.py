6616f124dd9241fbb123cc0617574347967188b3
Tomek Mrugalski <tomek@isc.org>
2025-02-13 22:02:51 +0100

    server code removed

--- server/app/controllers/__init__.py
+++
@@ -1,9 +0,0 @@
-import glob
-from os.path import dirname, basename, isfile, join
-
-# Default export of all controllers in directory
-# for one time import in routes.py.
-# In this directory should be placed only Flask controllers.
-modules = glob.glob(join(dirname(__file__), "*.py"))
-
-__all__ = [basename(f)[:-3] for f in modules if isfile(f)]
--- server/svarog-web.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from app import app
-
-app.config['SECRET_KEY'] = 'the earth is flat'
-
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
