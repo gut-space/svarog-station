e19bb32e501cf3026b1d867be4b6ef099168aba1
Tomek Mrugalski <tomek@isc.org>
2025-09-05 14:29:15 +0200

    satdump pipeline updated

--- station/recipes/satdump.py
+++ station/recipes/satdump.py
@@ -3,6 +3,7 @@ from contextlib import suppress
 import logging
 import os.path
 import signal
+import time
 import sh
 
 
@@ -30,36 +31,48 @@ def execute(working_dir: str, frequency: str, duration: timedelta, metadata, cus
     log_path = os.path.join(working_dir, sat_name + "-satdump.log")
 
     sdr_device = "airspy"
+    gain = 21
 
     # Use custom command if provided, otherwise use default
     if custom_command:
         cmd = custom_command
     else:
+
+        common_params = f"--samplerate 3e6 --frequency {frequency} --general_gain {gain} --baseband_format cf32 --source {sdr_device} --timeout {int(duration.total_seconds())}"
+
         match sat_name:
             case "noaa-15" | "noaa-18" | "noaa-19":
-                cmd = f"satdump live noaa_apt {working_dir} --samplerate 3e6 --frequency {frequency} --baseband_format cf32 " + \
-                      f"--source {sdr_device} --timeout {int(duration.total_seconds())}"
+                cmd = f"satdump live noaa_apt {working_dir} {common_params}"
             case "meteor-m2-3" | "meteor-m2-4":
-                cmd = f"satdump live meteor_m2_lrpt {working_dir} --samplerate 3e6 --frequency {frequency} --baseband_format cf32 " + \
-                      f"--source {sdr_device} --timeout {int(duration.total_seconds())}"
+                cmd = f"satdump live meteor_m2-x_lrpt {working_dir} {common_params}"
             case _:
-                cmd = f"satdump record {raw_path} --samplerate 3e6 --frequency {frequency} --baseband_format cf32 " + \
-                    f"--source {sdr_device} --timeout {int(duration.total_seconds())}"
+                cmd = f"satdump record {raw_path} {common_params}"
 
     cmd_bin = cmd.split()[0]
     cmd_args = cmd.split()[1:]
 
     logging.info(f"Executing command: {cmd_bin} {cmd_args}")
 
-    with suppress(sh.TimeoutException):
-        satdump_proc = sh.satdump( *cmd_args,
-            _timeout=duration.total_seconds(),
-            _timeout_signal=signal.SIGHUP,
+    satdump_proc = sh.satdump( *cmd_args,
+        _err=log_path
+    )
+    print(f"satdump_proc.type(): {type(satdump_proc)}")
+    print(f"satdump_proc: {satdump_proc}")
+    try:
+        satdump_proc.wait(duration.total_seconds()+15)
+    except sh.TimeoutException:
 
-            # rtl_fm and rx_fm both print messages on stderr
-            _err=log_path
-        )
-        satdump_proc.send_signal(signal.SIGKILL)
+        logging.info(f"Satdump process timed out after {duration.total_seconds()}+15 seconds")
+        logging.info(f"Satdump process is still running, sending SIGTERM")
+        if satdump_proc.is_alive():
+            satdump_proc.signal(signal.SIGTERM)
+            time.sleep(15)
+        if satdump_proc.is_alive():
+            logging.info(f"Satdump process is still running, sending SIGKILL")
+            satdump_proc.signal(signal.SIGKILL)
+
+        # Note: When using _timeout, sh returns a string result, not a RunningCommand object
+        # The timeout mechanism handles process termination automatically
 
     return [
         ("RAW", raw_path)
